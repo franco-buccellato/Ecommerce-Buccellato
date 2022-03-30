73725810b420367daaedd3963af2f5aa2393e714
franco-buccellato <francbuccellato@frba.utn.edu.ar>
2022-03-30 12:50:12 -0300

    Modificacion de creacion del Navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-    <div className="App">
-      <ItemListContainer greeting="Este es un mensaje de prueba."/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Ecommerce en Desarollo....
-        </p>
-      </header>
+    <div>
+      <NavBar/>
+      <div className="App">
+        <ItemListContainer greeting="Este es un mensaje de prueba."/>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Ecommerce en Desarollo....
+          </p>
+        </header>
+      </div>
     </div>
   );
 }
